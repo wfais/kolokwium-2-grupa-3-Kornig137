d28d460438909f5b56d778f6e05f55a0e388a175
Marcin <marcin.slonka@student.uj.edu.pl>
2025-01-23 09:08:32 +0100

    Zrobiono zadanie 5

--- ZADANIE5/main.py
+++ ZADANIE5/main.py
@@ -14,26 +14,34 @@ def create_app():
     2) Pole Entry
     3) Przycisk "Pokaż", który wyświetli wpisany tekst w innej etykiecie
     """
+
+    form = tk.Tk()
+
     # title - "Prosta aplikacja Tkinter"
+    form.title("Prosta aplikacja Tkinter")
 
     # label_instruct = umocuj przez pack
+    label_instrukt = tk.Label(form, text="Wpisz coś: ")
+    label_instrukt.pack(padx=5, pady=5)
 
     # entry_text = 
+    entry_text = tk.StringVar()
+    entry = tk.Entry(form, textvariable=entry_text)
+    entry.pack(padx=5, pady=5)
 
     # label_result = tk.Label(...
+    label_result = tk.Label(form, text="Tu pojawi się Twój tekst")
+    label_result.pack(padx=5, pady=5)
 
     # zdefiniuj funkcję show_text() pobierającą wpisany tekst i wyświetlającą w label_result
-    # def show_text():
-    #     ...
-    #     label_result.config(...)
+    def show_text():
+        label_result.config(text=f"Wpisałeś: {entry_text.get()}")
 
-    # button_show = 
-    # button_show.pack()
+    button_show = tk.Button(form, text="Załaduj", command=show_text)
+    button_show.pack(padx=5, pady=5)
 
-    # TODO: return root
-    pass
+    return form
 
 if __name__ == '__main__':
-    # TODO: app = create_app()
-    # TODO: app.mainloop()
-    pass
+    app = create_app()
+    app.mainloop()
